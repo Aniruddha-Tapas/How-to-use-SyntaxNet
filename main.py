43ba65038fc9a9f85f9807372d246902ece734b2
Aniruddha Tapas <aniruddhastapas@gmail.com>
2017-03-22 14:39:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,5 +23,5 @@ synonym = nlp_util.get_synonym(root)
 #print synonym
 
 #display parahrased sentence
-print "\nParaphrased sentence:"
+print "\nParaphrased sentence :\n"
 print nlp_util.get_paraphrase(input_str,root)
